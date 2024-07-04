f9ec2a411bf8dbdca1b9d09535204181029f18c9
mauricio <mauriciodm@gmail.com>
2024-07-04 09:03:20 -0300

    ngw

---
+++ modules/network/output.tf
@@ -0,0 +1,19 @@
+output "eks_subnet_public_la" {
+    value = aws_subnet.eks_subnet_public_la
+  
+}
+
+output "eks_subnet_public_lb" {
+    value = aws_subnet.eks_subnet_public_lb
+  
+}
+
+output "eks_subnet_private_la" {
+    value = aws_subnet.eks_subnet_private_la
+  
+}
+
+output "eks_subnet_private_lb" {
+    value = aws_subnet.eks_subnet_private_lb
+  
+}
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "subnet_pub_1a" {
+    value = module.eks_network.eks_subnet_public_la
+  
+}
